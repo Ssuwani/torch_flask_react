7982cf98de8e1190961619ef74bd7710030128b5
suwan <jsuwan961205@gmail.com>
2021-02-25 23:05:45 +0900

    Upload Project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
-
+import { Button, Input, CircularProgress } from '@material-ui/core'
+import axios from 'axios'
 function App() {
+
+  const [img, setImage] = useState([]);
+  const [imgurl, setImgurl] = useState();
+  const [result, setResult] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const onChange = (e) => {
+    setImage(e.target.files[0]);
+    setImgurl(URL.createObjectURL(e.target.files[0]))
+  }
+
+  const onClick = async () => {
+    setLoading(true);
+    const formData = new FormData();
+    formData.append('file', img);
+    // 서버의 upload API 호출
+    const res = await axios.post("http://localhost:5000/predict", formData);
+    setResult(res.data.class_name)
+    setLoading(false);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>React App for Image Classification</h2>
+        <Input type='file' onChange={onChange} />
+        <br />
+        <img src={imgurl} width='150px'/>
+        <br />
+        <Button onClick={onClick} variant="contained" color="primary">
+          Upload
+        </Button>
+        <br />
+        {
+          loading ? (
+            <CircularProgress />
+          ) :
+            result ?
+              <div>결과 : {result}</div>
+              :
+              <div></div>
+        }
       </header>
     </div>
   );
